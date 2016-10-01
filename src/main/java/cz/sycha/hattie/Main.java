6355ab9efe12817cc47b48bf0a15273d6e126773
Armen Kaleshian <armen@kriation.com>
2016-09-30 22:17:49 -0400

    Resolves issue #3 If the inventory is full on the helmet switch, the old helmet is dropped to the ground.

--- src/main/java/cz/sycha/hattie/Main.java
+++ src/main/java/cz/sycha/hattie/Main.java
@@ -1,7 +1,9 @@
 package cz.sycha.hattie;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
@@ -9,6 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
@@ -27,13 +31,27 @@ public class Main extends JavaPlugin {
 			PlayerInventory inv = player.getInventory();
 			ItemStack itemToUse = inv.getItemInMainHand();
 			ItemStack oldItem = inv.getHelmet();
+			int itemAmount = itemToUse.getAmount();
+			
+			if( itemAmount > 1 ) 
+			{ 
+				itemToUse.setAmount( 1 );
+			}
 			inv.setHelmet(itemToUse);
-			inv.removeItem(new ItemStack[] {itemToUse});
-
 			Material itemType = itemToUse.getType();
-			player.sendMessage(ChatColor.AQUA + itemType.name() + " has been put on your head!");
+			// player.sendMessage(ChatColor.AQUA + itemType.name() + " has been put on your head!");
+			
+			itemToUse.setAmount( itemAmount - 1 );
+			inv.setItemInMainHand( itemToUse );
+			
 			if(oldItem != null) {
-				inv.addItem(new ItemStack[] { oldItem });
+				HashMap<Integer,ItemStack> resMap = inv.addItem(oldItem);
+				if( !resMap.isEmpty() )
+				{
+					Location curLoc = player.getEyeLocation();
+					World curWorld = curLoc.getWorld();
+					curWorld.dropItem( curLoc, resMap.get( Integer.valueOf( 0 ) ) );
+				}
 			}
 		} catch(CommandException e) {
 			log.severe("CommandException: " + e.getMessage());
